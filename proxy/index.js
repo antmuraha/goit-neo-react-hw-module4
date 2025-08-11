c737145a12103f1f90ae5eea75cb28463d3356da
Ruslan Kryvko <ruslan.muraha19@gmail.com>
2025-08-12 02:55:22 +0300

    Refactor proxy to use ACCESS_KEY, update package.json and .gcloudignore, and adjust pnpm-lock.yaml

--- proxy/index.js
+++ proxy/index.js
@@ -1,11 +1,10 @@
 const ACCESS_KEY = process.env.ACCESS_KEY;
 
-export const pixabayProxy = async (req, res) => {
+export const proxy = async (req, res) => {
   res.set('Access-Control-Allow-Origin', process.env.NODE_ENV === 'production' ? 'https://antmuraha.github.io' : '*');
   res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.set('Access-Control-Allow-Headers', 'Content-Type');
   res.set('Accept-Version', 'v1');
-  res.set('Authorization', `Client-ID ${ACCESS_KEY}`);
 
   if (req.method === 'OPTIONS') {
     res.status(204).send('');
@@ -19,11 +18,19 @@ export const pixabayProxy = async (req, res) => {
 
   try {
     const queryParams = new URLSearchParams(req.query);
-    const url = `https://api.unsplash.com/?${queryParams}`;
+    const isSearch = !!queryParams.get('query');
+    const url = isSearch
+      ? `https://api.unsplash.com/search/photos?client_id=${ACCESS_KEY}&${queryParams}`
+      : `https://api.unsplash.com/photos?client_id=${ACCESS_KEY}&${queryParams}`;
+
     console.log(`Fetching from Unsplash: ${url}`);
+
+    // Fetch data from Unsplash API
     const response = await fetch(url);
     const data = await response.json();
-    res.status(response.status).json(data);
+    const xTotal = Number(response.headers.get('x-total'));
+    const xPerPage = Number(response.headers.get('x-per-page'));
+    res.status(response.status).json(isSearch ? data : { total: xTotal, total_pages: xPerPage, results: data });
   } catch (err) {
     console.error(err);
     res.status(500).send('Error fetching from Unsplash');
